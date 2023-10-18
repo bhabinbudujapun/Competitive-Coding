99b4c9292abaeb3c6bee5a8ea7c5c40c8f1cdfdc
bhabinpun <poon.bhabin6332@gmail.com>
2023-10-18 22:29:31 +0545

    Day-01 Set-up | Analysis Question

---
+++ 01Vaccine-Dates/01Vaccine-Dates.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+    for (int i = 0; i < 4; i++)
+	{
+		cout << "Hello world " << i << endl;
+	}
+	
+	return 0;
+}
